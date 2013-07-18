5a94aafbd25976fd7e1eaee8a1ae181289e6eaa6
henix <shellpick@gmail.com>
2013-07-18 17:15:26 +0800

    wc 对 stdin 可以只显示行数，不用 cut 了

--- lua/tagpost.sh
+++ lua/tagpost.sh
@@ -3,7 +3,7 @@
 [ $# -ne 3 ] && echo Usage: tagpost.sh tagsfile namefile timefile && exit 1
 
 tmp=$(mktemp)
-nlines=$(wc -l $1 | cut -d ' ' -f 1)
+nlines=$(cat $1 | wc -l)
 yes "$(cat $2)	$(cat $3)" | head -n $nlines > $tmp
 paste $1 $tmp
 rm $tmp
