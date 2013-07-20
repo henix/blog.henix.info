77604036d4364d16faf54d713cab36af625a8c93
henix <shellpick@gmail.com>
2013-07-20 21:08:25 +0800

    加入 links about guestbook 页面

--- lua/sync.sh
+++ lua/sync.sh
@@ -3,3 +3,6 @@ rsync -avz --delete blog ../static/
 rsync -avz --delete tags ../static/
 rsync -avz pages/index.html ../static/
 rsync -avz pages/search.html ../static/
+rsync -avz pages/links.html ../static/
+rsync -avz pages/about.html ../static/
+rsync -avz pages/guestbook.html ../static/
