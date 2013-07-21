4506fcb0b7e4edbb59988a34bd1b1bcc3afe0b2a
henix <shellpick@gmail.com>
2013-07-21 21:55:32 +0800

    处理文章评论数，在首页显示最多评论 Fix #2

--- lua/postlua.sh
+++ lua/postlua.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# name title catid catname publish_time postPath tagPath rela_path
-
 echo "{
 	name = '$(cat $1)',
 	title = $(cat $2 | ./escape.lua),
 	catid = '$(cat $3)',
 	catname = $(cat $4 | ./escape.lua),
 	publish_time = $(cat $5),
-	comment_count = 0,
-	post_path = '$6',
-	tags_path = '$7',
-	rela_path = '$8',
+	comment_count = $(cat $6),
+	post_path = '$7',
+	tags_path = '$8',
+	rela_path = '$9',
 }"
