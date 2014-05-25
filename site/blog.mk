ad99715af3fb9c381a9c9a5d8d6c4c8a8c6528db
henix <shellpick@gmail.com>
2014-05-25 19:48:01 +0800

    使用 pandoc 的 html5 输出格式

--- site/blog.mk
+++ site/blog.mk
@@ -15,11 +15,11 @@ _.cat:
 
 _.html: _.md _.id _.title _.date _.cat ../../_.sitetitle ../../_.siteurl ../../_.author ../../post.temp.htm ../../disqus.seg.htm ../../ga.seg.htm
 	echo PANDOC HTML $$(< _.id)
-	pandoc --mathjax $$([ -f _.toc ] && echo --toc) --template=../../post.temp.htm -A ../../disqus.seg.htm -A ../../ga.seg.htm -V id=$$(< _.id) -V "title=$$(< _.title)" -V date=$$(< _.date) -V cat=$$(< _.cat) -V "sitetitle=$$(< ../../_.sitetitle)" -V "siteurl=$$(< ../../_.siteurl)" -V "author=$$(< ../../_.author)" --css=../../root.css -o $@ $<
+	pandoc -t html5 --mathjax $$([ -f _.toc ] && echo --toc) --template=../../post.temp.htm -A ../../disqus.seg.htm -A ../../ga.seg.htm -V id=$$(< _.id) -V "title=$$(< _.title)" -V date=$$(< _.date) -V cat=$$(< _.cat) -V "sitetitle=$$(< ../../_.sitetitle)" -V "siteurl=$$(< ../../_.siteurl)" -V "author=$$(< ../../_.author)" --css=../../root.css -o $@ $<
 
 _.htm: _.md _.id ../../content.temp.htm
 	echo PANDOC HTM $$(< _.id)
-	pandoc --mathjax $$([ -f _.toc ] && echo --toc) --template=../../content.temp.htm -o $@ $<
+	pandoc -t html5 --mathjax $$([ -f _.toc ] && echo --toc) --template=../../content.temp.htm -o $@ $<
 
 %.svg: %.dot
 	dot -T svg $< > $@
