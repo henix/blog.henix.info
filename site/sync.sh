79ca2d124f952c2a6f6cf744f6c6e060c84a832f
henix <shellpick@gmail.com>
2014-07-25 13:21:23 +0800

    添加 3 篇旧文，添加问题研究分类

--- site/sync.sh
+++ site/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rsync -avz ./*.html ./*.css rss2.0.xml robots.txt ../output/
-rsync -avz --include="_.html" --exclude-from="-" works hacks views ../output/ <<EOF
+rsync -avz --include="_.html" --exclude-from="-" works hacks views dig ../output/ <<EOF
 *.swp
 _.*
 *.dot
