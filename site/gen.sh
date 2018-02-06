63978901d9e8c8593cd1933794314149580914e9
henix <shellpick@gmail.com>
2018-02-06 19:47:32 +0800

    添加 web 分类

--- site/gen.sh
+++ site/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for cat in tips security devtools program; do
+for cat in tips security devtools program web ; do
 	for i in $(find $cat -mindepth 1 -maxdepth 1 -type d); do
 		cd "$i"
 		make -s -f ../../blog.mk
