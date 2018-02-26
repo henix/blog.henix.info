cb93874aa8e64f68f7b667c34de3ec3c16e77eff
henix <shellpick@gmail.com>
2018-02-26 19:52:52 +0800

    恢复部分文章

--- site/gen.sh
+++ site/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for cat in tips security devtools program web ; do
+for cat in tips security doc program web cs linux fp ; do
 	for i in $(find $cat -mindepth 1 -maxdepth 1 -type d); do
 		cd "$i"
 		make -s -f ../../blog.mk
