d5bbd546f2c34cbf626e1fe88859dd34fbd909a2
henix <shellpick@gmail.com>
2019-07-12 18:06:02 +0800

    sync 时默认所有目录

--- site/sync.sh
+++ site/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rsync -avz ./*.html ./*.css ./*.js ./*.jpg rss2.0.xml robots.txt ../output/
-rsync -avz --exclude-from="-" tips/* security/* doc/* program/* web/* cs/* linux/* fp/* ../output/blog/ <<EOF
+rsync -avz --exclude-from="-" ./*/* ../output/blog/ <<EOF
 *.swp
 _.*
 *.dot
