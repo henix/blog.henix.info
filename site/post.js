02366958382ba1d5ce0778f0bab4ce8411fa0868
henix <shellpick@gmail.com>
2019-10-17 21:16:07 +0800

    实现图片 lazy load fix #15

---
+++ site/post.js
@@ -0,0 +1,27 @@
+// img lazy-load
+(function() {
+	var observer = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			// 因为最开始当 intersectionRatio = 0 时也会触发回调，故必须加上判断
+			if (entry.intersectionRatio > 0) {
+				var elem = entry.target;
+				observer.unobserve(elem);
+				var src = elem.getAttribute("data-src");
+				elem.removeAttribute("data-src");
+				if (window.fetch && window.URL && URL.createObjectURL) {
+					fetch(src).then(function(resp) {
+						return resp.blob();
+					}).then(function(blob) {
+						var url = URL.createObjectURL(blob);
+						elem.setAttribute("src", url);
+					});
+				} else {
+					elem.setAttribute("src", src);
+				}
+			}
+		});
+	}, { threshold: 0.05 });
+	[].slice.call(document.querySelectorAll("img[data-src]")).forEach(function(elem) {
+		observer.observe(elem);
+	});
+})();
--- site/sync.sh
+++ site/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rsync -avz ./*.html ./*.css ./*.js ./*.jpg rss2.0.xml robots.txt ../public/
+rsync -avz ./*.html ./*.css ./valid.js ./post.js ./*.jpg rss2.0.xml robots.txt ../public/
 rsync -avz --exclude-from="-" ./*/* ../public/blog/ <<EOF
 *.swp
 _.*
