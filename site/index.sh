44f34bebbd98ca548b15c6485d532f5f5bad40fc
henix <shellpick@gmail.com>
2019-10-17 17:03:16 +0800

    添加 lang 属性 fix #14

--- site/index.sh
+++ site/index.sh
@@ -2,7 +2,7 @@
 . ./_.ini
 cat <<EOF
 <!DOCTYPE html>
-<html>
+<html lang="zh-Hans-CN">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
