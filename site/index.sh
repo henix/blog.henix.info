db5040cedbf38ad0561a93970059b6cea7334e9b
henix <shellpick@gmail.com>
2022-09-18 10:35:21 +0800

    使用 html5 标准 charset 声明

--- site/index.sh
+++ site/index.sh
@@ -4,7 +4,7 @@ cat <<EOF
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 <title>$sitetitle</title>
 EOF
