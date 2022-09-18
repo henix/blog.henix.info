c7582368b421e51895f055979a07a96c0778ac99
henix <shellpick@gmail.com>
2022-09-18 10:38:06 +0800

    简化 viewport 声明

--- site/index.sh
+++ site/index.sh
@@ -5,7 +5,7 @@ cat <<EOF
 <html lang="zh-CN">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>$sitetitle</title>
 EOF
 cat myhead.seg.htm
